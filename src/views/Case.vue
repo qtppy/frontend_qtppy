f84b8a9de93fa21e85a41ea3d117bd6b41e130f2
lengyaohui <lengyaohui@163.com>
2020-06-24 18:15:35 +0800

    .

--- src/views/Case.vue
+++ src/views/Case.vue
@@ -57,34 +57,6 @@
 			</el-pagination>
 		</el-col>
 
-		<!-- 编辑界面
-		<el-dialog title="编辑" v-model="editFormVisible" :close-on-click-modal="false">
-			<el-form :model="editForm" label-width="80px" :rules="editFormRules" ref="editForm">
-				<el-form-item label="姓名" prop="name">
-					<el-input v-model="editForm.name" auto-complete="off"></el-input>
-				</el-form-item>
-				<el-form-item label="性别">
-					<el-radio-group v-model="editForm.sex">
-						<el-radio class="radio" :label="1">男</el-radio>
-						<el-radio class="radio" :label="0">女</el-radio>
-					</el-radio-group>
-				</el-form-item>
-				<el-form-item label="年龄">
-					<el-input-number v-model="editForm.age" :min="0" :max="200"></el-input-number>
-				</el-form-item>
-				<el-form-item label="生日">
-					<el-date-picker type="date" placeholder="选择日期" v-model="editForm.birth"></el-date-picker>
-				</el-form-item>
-				<el-form-item label="地址">
-					<el-input type="textarea" v-model="editForm.addr"></el-input>
-				</el-form-item>
-			</el-form>
-			<div slot="footer" class="dialog-footer">
-				<el-button @click.native="editFormVisible = false">取消</el-button>
-				<el-button type="primary" @click.native="editSubmit" :loading="editLoading">提交</el-button>
-			</div>
-		</el-dialog> -->
-
 		<!-- 新增测试用例界面 -->
 		<el-dialog :fullscreen="true" :visible="addCaseVisible" :close-on-click-modal="false" @close="addCaseVisible = false">
       <template slot="title">
@@ -469,8 +441,8 @@
 				</el-row>
         <el-row>
           <el-tabs v-model="responseBodyTab" size="mini" v-loading="responseLoading"     
-          element-loading-text="Send Request..."
-          element-loading-spinner="el-icon-loading">
+            element-loading-text="Send Request..."
+            element-loading-spinner="el-icon-loading">
             <!-- Response body-->
             <el-tab-pane label="Body" name="responseBody">
               <el-input
@@ -514,7 +486,29 @@
 											</template>
 										</el-table-column>
 								</el-table>
-            </el-tab-pane>        
+            </el-tab-pane>
+            <!-- Response 状态 -->
+            <el-tab-pane name="responseStatus" scope-slot>
+              <template slot="label">
+                <i>　　　　　　　　　　　　</i>
+                <el-link type="info" :underline="false" disabled>
+                  Status: {{this.responseStatus.status}}
+                  <i v-if="this.responseStatus.status === 200" class="el-icon-circle-check" style="color:#67C23A;"></i>
+                  <i v-else class="el-icon-circle-close" style="color:#F56C6C;"></i>
+                </el-link>
+
+                <el-link type="info" :underline="false" disabled>
+                  Time: 
+                  <i style="color:#67C23A;">{{this.responseStatus.time}} ms</i>
+                </el-link>
+
+
+                <el-link type="info" :underline="false" disabled>
+                  Size: 
+                  <i style="color:#67C23A;">{{this.responseStatus.size}} kb</i>
+                </el-link>
+              </template> 
+            </el-tab-pane>
           </el-tabs>
         </el-row>
 			</el-form>
@@ -925,7 +919,13 @@
          * @var responseHeaderData 响应头数据
          * @var responseLoading 响应加载
          * @var responseReadOnly 响应文本只读
+         * @var responseStatus 响应状态
          */
+        responseStatus: {
+          status: 200,
+          time: 26,
+          size: 1000
+        },
         responseBodyTab: 'responseBody',
         HeadersResTextArea: '',
         responseHeaderData: [
